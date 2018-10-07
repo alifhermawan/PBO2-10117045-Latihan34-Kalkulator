790d1593fe3b1f0e06a22d95c0ccc108e7b06e95
alifhermawan <43422583+alifhermawan@users.noreply.github.com>
2018-10-07 09:36:53 +0800

    Tugas 3

---
+++ PBO2-10117045-Latihan34-Kalkulator/src/pbo2/pkg10117045/latihan34/kalkulator/Kalkulator.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117045.latihan34.kalkulator;
+
+/**
+ *
+ * @author 
+ * NAMA     : Alif Hermawan
+ * KELAS    : PBO2
+ * NIM      : 10117045
+ * Deskripsi Program : Program ini berisi perintah untuk menampilkan hasil  
+ * operasi aritmetik dari dua angka yang dimasukan user
+ */
+public class Kalkulator {
+    public double value1;
+    public double value2;
+
+    public double tambahBilangan(){ return value1+value2;}
+    public double kurangBiangan(){ return value1-value2;}
+    public double kaliBilangan(){ return value1*value2;}
+    public double bagiBilangan(){ return value1/value2;}
+    public double sisaBilangan(){ return value1%value2;}
+
+
+
+}
---
+++ PBO2-10117045-Latihan34-Kalkulator/src/pbo2/pkg10117045/latihan34/kalkulator/Utama.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117045.latihan34.kalkulator;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * @author 
+ * NAMA     : Alif Hermawan
+ * KELAS    : PBO2
+ * NIM      : 10117045
+ * Deskripsi Program : Program ini berisi perintah untuk menampilkan hasil  
+ * operasi aritmetik dari dua angka yang dimasukan user
+ *
+ */
+public class Utama {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        Kalkulator kal = new Kalkulator();
+
+        System.out.println("====Aplikasi Kalkulator Bilangan====");
+        System.out.print("Masukkan Angka ke-1 : ");
+        kal.value1 = scn.nextDouble();
+        System.out.print("Masukkan Angka ke-2 : ");
+        kal.value2 = scn.nextDouble();
+
+        //format dua digit di belakang koma
+        DecimalFormat df = new DecimalFormat("###.##");
+
+        System.out.println("\nHasil Penjumlahan : "+kal.tambahBilangan());
+        System.out.println("Hasil Pengurangan : "+kal.kurangBiangan());
+        System.out.println("Hasil Perkalian : "+kal.kaliBilangan());
+        System.out.println("Hasil Pembagian : "+df.format(kal.bagiBilangan()));
+        System.out.println("Hasil Sisa : "+kal.sisaBilangan());
+
+    }
+}
